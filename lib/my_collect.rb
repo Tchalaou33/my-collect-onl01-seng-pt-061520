3d7a552bd5e47f0c43b693f5eaa10f13ba617819
Andrea Tchalaou <attchalaou@gmail.com>
2020-06-28 19:56:59 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,14 @@
-list = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+# list = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
-def my_collect(array)
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collect(array) do |name|
-  name.split(" ").first
-end
-end
+# def my_collect(array)
+# array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+# my_collect(array) do |name|
+#   name.split(" ").first
+# end
+# end
 
 # collection = ['ruby', 'javascript', 'python', 'objective-c']
 # my_collect(collection) do |lang|
 #   lang.upcase
 # end
+
